9104b11f0506112e3b8f7607ee4915c4dd3bef08
tahir@vurso.co.uk <tahir@vurso.co.uk>
2019-10-17 17:12:41 +0100

    Initial commit

---
+++ Interview/InMemoryRepoTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview
+{
+    [TestFixture]
+    public class InMemoryRepoTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            // setup anything required for the tests
+        }
+
+        [Test]
+        public void First_Test_Place_Holder()
+        {
+            // TODO: Configure any startups and builders
+            // TODO: GWT naming convention for tests
+            // TODO: Ensure correct coverage
+        }
+    }
+}
